889fa4d5580b810bbc8d94a6157c1e8430ac962b
SicaUncle911 <67003013+SicaUncle911@users.noreply.github.com>
2021-06-08 09:07:26 +0800

    Delete day01 directory

--- day01/js/test01.js
+++
@@ -1,4 +0,0 @@
-// 网页中使用外部文件的JavaScript代码
-(function method2(){
-	console.log('我是外部文件的JavaScript代码');
-})();
--- day01/js/test03.js
+++
@@ -1,6 +0,0 @@
-// 网页中使用外部文件的JavaScript代码
-function method5(){
-	console.log('我是head标签中使用的JavaScript文件');
-};
-// setTimeout(method5,5000);
-method5();
